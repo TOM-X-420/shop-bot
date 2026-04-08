612180c4ba23d7455cb64603372213377fde52fe
TOM-X-420 <parveztg142@gmail.com>
2026-04-08 23:13:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 
 API_TOKEN = '8702965570:AAGGPb9UcroWCO_6rJZ95ykqGwUEkZigrm0'
 
-bot = telebot.TeleBot(API_TOKEN)
+bot = telebot.TeleBot(8702965570:AAGGPb9UcroWCO_6rJZ95ykqGwUEkZigrm0)
 
 if __name__ == '__main__':
     bot.polling()
